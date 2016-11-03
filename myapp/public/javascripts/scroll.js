76c17d556f3cbb014026a36c82eed117172f374d
Daniel Gott <dannygott98@gmail.com>
2016-11-03 01:27:44 +0000

    routing and other such things

--- myapp/public/javascripts/scroll.js
+++ myapp/public/javascripts/scroll.js
@@ -1,4 +1,7 @@
 
+var depth = 0
+var loaded = "none"
+/*
 function scrollfirst() {
   $('html,body').animate({
       scrollTop: $(".lowercontent").offset().top},
@@ -12,17 +15,42 @@ function scrollfirst() {
       });
   });
 
+*/
+  window.onload = function (event) {
+    window.location.hash = "#home";
+  };
+
   function fadetoPhoto() {
       $(".maintarg").fadeOut( 'slow', function(){
         $(".splendidChap").fadeIn( 'slow', function(){
-          $(".log").text('Fade In Transition Complete');
+            window.location.hash = "#splendidChap";
+            loaded = ".splendidChap"
         });
       });
   }
   function fadetoDeveloper() {
       $(".maintarg").fadeOut( 'slow', function(){
         $(".developer").fadeIn( 'slow', function(){
-          $(".log").text('Fade In Transition Complete');
+          window.location.hash = "#developer";
+          loaded = ".developer"
         });
       });
   }
+
+  window.onhashchange = function(){
+    switch(location.hash) {
+      case '#home':
+        loadhome()
+      break;
+      case '#has2':
+      break;
+    }
+  }
+  
+function loadhome(argument) {
+   $(loaded).fadeOut( 'slow', function(){
+           $(".maintarg").fadeIn( 'slow', function(){
+          console.log("i ran")
+        });
+      });
+}

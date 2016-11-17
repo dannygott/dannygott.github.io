3691f6b9fea05e36b86a1bc6bc8b6044c7972f33
dannygott <dannygott98@gmail.com>
2016-11-16 20:45:03 -0500

    started on portpholio

--- myapp/public/javascripts/scroll.js
+++ myapp/public/javascripts/scroll.js
@@ -22,10 +22,13 @@ function scrollfirst() {
   function fadetoPhoto() {
     if(loaded == "none"){
       $(".maintarg").fadeOut( 'slow', function(){
-        $(".splendidChap").fadeIn( 'slow', function(){
-            window.location.hash = "#splendidChap";
-            loaded = ".splendidChap"
+        $(".Photographer").fadeIn( 3000, function(){
+            window.location.hash = "#Photographer";
+            loaded = ".Photographer"
         });
+        $(".Photoglide").animate({
+            top: "-=50vw",
+          }, 1000 );
       });
     }
   }
@@ -39,11 +42,22 @@ function scrollfirst() {
       });
     }
   }
+  function fadetoChap() {
+    if(loaded == "none"){
+      $(".maintarg").fadeOut( 'slow', function(){
+        $(".splendidChap").fadeIn( 'slow', function(){
+          window.location.hash = "#splendidChap";
+          loaded = ".splendidChap"
+        });
+      });
+    }
+  }
 
   window.onhashchange = function(){
     switch(location.hash) {
       case '#home':
         loadhome()
+
       break;
       case '#has2':
       break;
@@ -52,6 +66,7 @@ function scrollfirst() {
 
 function loadhome() {
   if(loaded != "none" && checkhover() != "A"){
+    resetanimation()
    $(loaded).fadeOut( 'slow', function(){
            $(".maintarg").fadeIn( 'slow', function(){
           loaded = "none"
@@ -61,8 +76,6 @@ function loadhome() {
   }
 }
 
-
-
 function checkhover(){
  var element = $(':hover');
     if(element.length)
@@ -70,7 +83,11 @@ function checkhover(){
         var domElement = element[element.length - 1];
         var tagName = domElement.tagName;
         return tagName
-
-
     }
+}
+function resetanimation() {
+  $(".Photoglide").animate({
+      top: "+=50vw",
+    }, 2 );
+
 }
